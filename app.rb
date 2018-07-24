9775a88baccb3b30add2ebb918330f803b714ff6
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-24 05:34:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new
+      @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |info|
         Ship.new(info)
