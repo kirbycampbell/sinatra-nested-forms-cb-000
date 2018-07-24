f30da8959ab8a12362bbe3066c7f5a96758d54b7
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-24 05:08:32 +0000

    automatically backed up by learn

--- models/ship.rb
+++ models/ship.rb
@@ -1,19 +1,19 @@
 class Ship
   attr_accessor :name, :type, :booty
-  @@all = []
+  SHIPS = []
   def initialize(args)
     @name = args[:name]
     @type = args[:type]
     @booty = args[:booty]
-    @@all << self
+    SHIPS << self
   end
 
   def self.all
-    @@all
+    SHIPS
   end
 
   def self.clear
-    @@all.clear
+    SHIPS.clear
   end
 
 end
