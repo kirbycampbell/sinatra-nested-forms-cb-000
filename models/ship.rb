43d49859be8b53146cab30bbe720ed5e92d88fcf
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-24 05:11:28 +0000

    automatically backed up by learn

--- models/ship.rb
+++ models/ship.rb
@@ -1,10 +1,10 @@
 class Ship
   attr_accessor :name, :type, :booty
   SHIPS = []
-  def initialize(args)
-    @name = args[:name]
-    @type = args[:type]
-    @booty = args[:booty]
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
     SHIPS << self
   end
 
